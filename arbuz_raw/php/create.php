cf6fb81100184e5905ffbcf8ca05b1e54a4a8e64
Ira <inovykov@e1r6p5.unit.ua>
2018-04-07 12:32:55 +0300

    initial htmllll  shop

---
+++ arbuz_raw/php/create.php
@@ -0,0 +1,34 @@
+<?php
+
+$file = 'private/passwd';
+if (file_exists($file) === FALSE)
+	mkdir('private');
+if ($_POST['login'] !== "" && $_POST['passwd'] !== "" && $_POST['submit'] == 'OK')
+{
+	$arr['login'] = $_POST['login'];
+	$arr['passwd'] = hash('whirlpool', $_POST['passwd']);
+	$file_cont = unserialize(file_get_contents($file));
+	foreach ($file_cont as $key => $value)
+	{
+		if ($value['login'] === $_POST['login'])
+		{
+			echo "ERROR\n";
+			$i = 1;
+			return ;
+		}
+	}
+	if ($i !== 1)
+	{
+		$file_cont[] = $arr;
+		echo "OK\n";
+	}
+	$serializedData = serialize($file_cont);
+	file_put_contents($file, $serializedData);
+}
+else
+{
+	echo "ERROR\n";
+	return ;
+}
+
+?>
